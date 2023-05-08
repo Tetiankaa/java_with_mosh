8d6ca9d945a0e53edb18696904f9cdb894d9845b
Tetiankaa <tanyakorsun0208@gmail.com>
2023-05-08 21:21:48 +0200

    Initial commit

---
+++ src/main/java/com/example/java_with_mosh/JavaWithMoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_with_mosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithMoshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWithMoshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_with_mosh/JavaWithMoshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_with_mosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWithMoshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
